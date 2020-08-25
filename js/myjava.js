f939e30c8c788eab2eec0cd279b922635a550b8a
RaniaAbdullahh <raniaabdullah6600@gmail.com>
2020-08-25 22:00:16 +0300

    adding js code

---
+++ js/myjava.js
@@ -0,0 +1,28 @@
+var greeting = "Welcome to our store Dears";
+
+alert(greeting);
+
+console.log(greeting);
+
+var cond = prompt(" do you have a Pet?");
+var ans;
+
+
+
+if (cond == "yes") {
+    ans = 'you will find you Pet needs here';
+
+ } else if(cond == "no") {
+     ans = 'you should have a Pet;)';
+ } 
+
+ 
+ document.write('<h2>'+ans+'</h2>');
+
+ console.log(ans);
+
+ confirm("Press a button!\nEither OK or Cancel.");
+
+
+
+
