3daa780e18b290a19e4934e485b39426b7c5029b
RaniaAbdullahh <raniaabdullah6600@gmail.com>
2020-08-26 14:24:51 +0300

    adding function

--- js/myjava.js
+++ js/myjava.js
@@ -24,5 +24,24 @@ if (cond == "yes") {
  confirm("Press a button!\nEither OK or Cancel.");
 
 
+ var choice = function(){
 
+    var userinput = prompt('do you like cat or dog?');
+    
+    var userchoice;
+
+    if (userinput == "cat"){
+
+        userchoice = "<img src='images/GettyImages-1199242002-1-1920x1080.jpg' alt=''>";
+    }
+    else if (userinput == "dog"){
+        userchoice = "<img src='images/5ec6c6dc988ee362762f137b.jpg' alt=''>";
+
+    }
+    return userchoice;
+
+
+    }
+
+ 
 
